14541492432f569d580a2f2fb26decd1ac3833c9
rinnv <rinnv@vnext.com.vn>
2020-09-08 14:53:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+       welcome to react hook
+       <h1>welcome</h1>
+    </div>
+  );
+}
+
+export default App;
