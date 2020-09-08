d4f79735942a9ec1d2dd406a7063035e8ce2d8d7
rinnv <rinnv@vnext.com.vn>
2020-09-08 17:11:52 +0700

    useStates todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-
+// import ColorBox from './components/ColorBox';
+import TodoList from './components/ToDoList';
 function App() {
+  const [todoList,setTodoList]= useState([
+    {id:1,title:"mootj"},
+    {id:2,title:"hai"},
+    {id:3,title:"ba"}
+  ])
+function handleTodoClick(todo){
+    const index= todoList.findIndex(x => x.id === todo.id);
+    if(index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index,1);
+    setTodoList(newTodoList);
+}
   return (
     <div className="app">
-       welcome to react hook
-       <h1>welcome</h1>
+       <h1>React to do list</h1>
+       {/* <ColorBox/> */}
+       <TodoList todos={todoList} onTodoClick={handleTodoClick}></TodoList>
     </div>
   );
 }
